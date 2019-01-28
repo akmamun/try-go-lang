d88d7a5feb26d97b14fb6645b93e1d8ba0b777fb
mrxmamun <almamunkhan04@gmail.com>
2019-01-29 03:21:12 +0600

    condition, for loop and contastant

--- constants.go
+++ constants.go
@@ -1,8 +1,15 @@
 package main
 
 import "fmt"
+import "math"
 func main() {
-	const n =  0.0004
+	const n = 6000000
 	const d = 3e20 / n
-    fmt.Println(d)
+	fmt.Println(d)
+
+	fmt.Println(int64(d)) 
+	// math calculation
+	fmt.Println(math.Sin(n))
+	fmt.Println(math.Cos(n))
+
 }
---
+++ for.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+	// for n := 0; n < 10; n++ {
+	// 	fmt.Println(n)
+	// }
+	i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+}
